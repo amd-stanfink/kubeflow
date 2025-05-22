97b8d8335f188600aa81eb49c0a5bfd0b3e9ec0e
Stanislau Fink <stanfink@amd.com>
2025-05-22 11:23:50 +0200

    Entrypoint for managing permissions added

--- components/example-notebook-servers/jupyter-rocm/entrypoint.sh
+++ components/example-notebook-servers/jupyter-rocm/entrypoint.sh
@@ -4,7 +4,7 @@ getent group $RENDER_GROUP_ID || groupadd --gid $RENDER_GROUP_ID render_host
 
 usermod -aG $RENDER_GROUP_ID $NB_USER
 
-newgrp $(getent group $RENDER_GROUP_ID | cut -d':' -f 1)
+exec newgrp $(getent group $RENDER_GROUP_ID | cut -d':' -f 1)
 
 chmod u-s /usr/sbin/groupadd /usr/sbin/usermod /usr/bin/chmod
 
