123584d45b5feac3c83404bade734e57c757fb40
Stanislau Fink <stanfink@amd.com>
2025-05-22 14:21:28 +0200

    Entrypoint for managing permissions added

--- components/example-notebook-servers/jupyter-rocm/entrypoint.sh
+++ components/example-notebook-servers/jupyter-rocm/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+# Add render group if it doesn't exist
 getent group $RENDER_GROUP_ID || groupadd --gid $RENDER_GROUP_ID render_host
 
+# Add user to the group
 usermod -aG $RENDER_GROUP_ID $NB_USER
 
-exec newgrp $(getent group $RENDER_GROUP_ID | cut -d':' -f 1)
-
-chmod u-s /usr/sbin/groupadd /usr/sbin/usermod /usr/bin/chmod
-
-exec /init
+# Reload group membership without logging out/in by starting a new shell
+# that preserves the environment and executes the init process
+sg $(getent group $RENDER_GROUP_ID | cut -d':' -f 1) "chmod u-s /usr/sbin/groupadd /usr/sbin/usermod /usr/bin/chmod && exec /init"
